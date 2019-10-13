09413c0f186cde52471c01b597c5f9f6295972dd
Oscar Mañas <oscmansan@gmail.com>
2019-10-13 20:20:15 +0200

    w2 mandatory task first working implementation

--- w2/start.m
+++ w2/start.m
@@ -14,37 +14,38 @@ mask_dst=logical(imread('mask_dst_eyes.png'));
 for nC = 1: nChannels
     
     %TO DO: COMPLETE the ??
-    drivingGrad_i = ??
-    drivingGrad_j = ??
+    [gx, gy] = gradient(src(:,:,nC));
+    drivingGrad_i = gx;
+    drivingGrad_j = gy;
 
-    driving_on_src = ??
+    driving_on_src = divergence(drivingGrad_i, drivingGrad_j);
     
-    driving_on_dst = zeros(size(src(:,:,1)));   
+    driving_on_dst = zeros(size(src(:,:,1)));
     driving_on_dst(mask_dst(:)) = driving_on_src(mask_src(:));
     
     param.driving = driving_on_dst;
 
-    dst1(:,:,nC) = sol_Poisson_Equation_Axb(dst(:,:,nC), mask_dst,  param);
+    dst1(:,:,nC) = sol_Poisson_Equation_Axb(dst(:,:,nC), mask_dst, param);
 end
 
-%Mouth
 %masks to exchange: Mouth
 mask_src=logical(imread('mask_src_mouth.png'));
 mask_dst=logical(imread('mask_dst_mouth.png'));
 for nC = 1: nChannels
     
     %TO DO: COMPLETE the ??
-    drivingGrad_i = ??
-    drivingGrad_j = ??
+    [gx, gy] = gradient(src(:,:,nC));
+    drivingGrad_i = gx;
+    drivingGrad_j = gy;
 
-    driving_on_src = ??
+    driving_on_src = divergence(drivingGrad_i, drivingGrad_j);
     
-    driving_on_dst = zeros(size(src(:,:,1)));  
+    driving_on_dst = zeros(size(src(:,:,1)));
     driving_on_dst(mask_dst(:)) = driving_on_src(mask_src(:));
     
     param.driving = driving_on_dst;
 
-    dst1(:,:,nC) = sol_Poisson_Equation_Axb(dst1(:,:,nC), mask_dst,  param);
+    dst1(:,:,nC) = sol_Poisson_Equation_Axb(dst1(:,:,nC), mask_dst, param);
 end
 
 imshow(dst1/256)
