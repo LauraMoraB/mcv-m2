8cbffffab338aa96da9353040b117f57a5f21e01
Oscar Mañas <oscmansan@gmail.com>
2019-10-15 21:32:35 +0200

    Implement mixing gradients method

--- w2/start.m
+++ w2/start.m
@@ -14,9 +14,8 @@ mask_dst=logical(imread('mask_dst_eyes.png'));
 for nC = 1: nChannels
     
     %TO DO: COMPLETE the ??
-    [gx, gy] = gradient(src(:,:,nC));
-    drivingGrad_i = gx;
-    drivingGrad_j = gy;
+    %[drivingGrad_i, drivingGrad_j] = importGradients(src(:,:,nC));
+    [drivingGrad_i, drivingGrad_j] = mixGradients(src(:,:,nC), dst(:,:,nC));
 
     driving_on_src = divergence(drivingGrad_i, drivingGrad_j);
     
@@ -31,12 +30,12 @@ end
 %masks to exchange: Mouth
 mask_src=logical(imread('mask_src_mouth.png'));
 mask_dst=logical(imread('mask_dst_mouth.png'));
+
 for nC = 1: nChannels
     
     %TO DO: COMPLETE the ??
-    [gx, gy] = gradient(src(:,:,nC));
-    drivingGrad_i = gx;
-    drivingGrad_j = gy;
+    %[drivingGrad_i, drivingGrad_j] = importGradients(src(:,:,nC));
+    [drivingGrad_i, drivingGrad_j] = mixGradients(src(:,:,nC), dst(:,:,nC));
 
     driving_on_src = divergence(drivingGrad_i, drivingGrad_j);
     
@@ -49,3 +48,28 @@ for nC = 1: nChannels
 end
 
 imshow(dst1/256)
+
+
+function [drivingGrad_i, drivingGrad_j] = importGradients(src)
+    [srcGrad_i, srcGrad_j] = gradient(src);
+    drivingGrad_i = srcGrad_i;
+    drivingGrad_j = srcGrad_j;
+end
+
+function [drivingGrad_i, drivingGrad_j] = mixGradients(src, dst)
+    [srcGrad_i, srcGrad_j] = gradient(src);
+    [dstGrad_i, dstGrad_j] = gradient(dst);
+    cond = magnitude(srcGrad_i, srcGrad_j) > magnitude(dstGrad_i, dstGrad_j);
+    drivingGrad_i = where(cond, srcGrad_i, dstGrad_i);
+    drivingGrad_j = where(cond, srcGrad_j, dstGrad_j);
+end
+
+function [result] = magnitude(gx, gy)
+    result = sqrt(gx.^2 + gy.^2);
+end
+
+function [result] = where(cond, x, y)
+    result = zeros(size(cond));
+    result(cond) = x(cond);
+    result(~cond) = y(~cond);
+end
