79d03ea74a47f081bb7ba430783c54f3658cde63
Oscar Mañas <oscmansan@gmail.com>
2019-10-31 01:18:50 +0100

    w3 mandatory task first implementation

--- w3/sol_ChanVeseIpol_GDExp.m
+++ w3/sol_ChanVeseIpol_GDExp.m
@@ -30,15 +30,16 @@ while dif>tol && nIter<iterMax
     
     
     %Fixed phi, Minimization w.r.t c1 and c2 (constant estimation)
-    c1 = ??; %TODO 1: Line to complete
-    c2 = ??; %TODO 2: Line to complete
+    H = heavisideReg(phi, epHeaviside);
+    c1 = sum(I.*H) / sum(H); %TODO 1: Line to complete
+    c2 = sum(I.*(1-H)) / sum(1-H); %TODO 2: Line to complete
     
     %Boundary conditions
-    phi(1,:)   = ??; %TODO 3: Line to complete
-    phi(end,:) = ??; %TODO 4: Line to complete
+    phi(1,:)   = phi(2,:); %TODO 3: Line to complete
+    phi(end,:) = phi(end-1,:); %TODO 4: Line to complete
 
-    phi(:,1)   = ??; %TODO 5: Line to complete
-    phi(:,end) = ??; %TODO 6: Line to complete
+    phi(:,1)   = phi(:,2); %TODO 5: Line to complete
+    phi(:,end) = phi(:,end-1); %TODO 6: Line to complete
 
     
     %Regularized Dirac's Delta computation
@@ -46,25 +47,33 @@ while dif>tol && nIter<iterMax
     
     %derivatives estimation
     %i direction, forward finite differences
-    phi_iFwd  = ??; %TODO 7: Line to complete
-    phi_iBwd  = ??; %TODO 8: Line to complete
+    phi_iFwd  = DiFwd(phi, hi); %TODO 7: Line to complete
+    phi_iBwd  = DiBwd(phi, hi); %TODO 8: Line to complete
     
     %j direction, forward finitie differences
-    phi_jFwd  = ??; %TODO 9: Line to complete
-    phi_jBwd  = ??; %TODO 10: Line to complete
+    phi_jFwd  = DjFwd(phi, hj); %TODO 9: Line to complete
+    phi_jBwd  = DjBwd(phi, hj); %TODO 10: Line to complete
     
     %centered finite diferences
-    phi_icent   = ??; %TODO 11: Line to complete
-    phi_jcent   = ??; %TODO 12: Line to complete
+    phi_icent   = (phi_iFwd + phi_iBwd) / 2; %TODO 11: Line to complete
+    phi_jcent   = (phi_jFwd + phi_jBwd) / 2; %TODO 12: Line to complete
     
     %A and B estimation (A y B from the Pascal Getreuer's IPOL paper "Chan
     %Vese segmentation
-    A = ??; %TODO 13: Line to complete
-    B = ??; %TODO 14: Line to complete
+    A = mu ./ (sqrt(eta^2 + phi_iFwd.^2 + phi_jcent.^2)); %TODO 13: Line to complete
+    B = mu ./ (sqrt(eta^2 + phi_icent.^2 + phi_jFwd.^2)); %TODO 14: Line to complete
     
     
     %%Equation 22, for inner points
-    phi(??) = ??; %TODO 15: Line to complete
+    for i = 2:ni-1
+        for j = 2:nj-1
+            phi(i,j) = (phi(i,j) + dt*delta_phi(i,j)* ...
+                (A(i,j)*phi(i+1,j) + A(i-1,j)*phi(i-1,j) + ...
+                B(i,j)*phi(i,j+1) + B(i,j-1)*phi(i,j-1) - ...
+                nu - lambda1*((I(i,j)-c1)^2) + lambda2*((I(i,j)-c2)^2))) / ...
+                (1 + dt*delta_phi(i,j)*(A(i,j) + A(i-1,j) + B(i,j) + B(i, j-1))); %TODO 15: Line to complete
+        end
+    end
     
             
     %Reinitialization of phi
@@ -84,26 +93,28 @@ while dif>tol && nIter<iterMax
     %looking for.
     dif = mean(sum( (phi(:) - phi_old(:)).^2 ))
           
-    %Plot the level sets surface
-    subplot(1,2,1) 
-        %The level set function
-        surfc(??)  %TODO 16: Line to complete 
-        hold on
-        %The zero level set over the surface
-        contour(??); %TODO 17: Line to complete
-        hold off
-        title('Phi Function');
-    
-    %Plot the curve evolution over the image
-    subplot(1,2,2)
-        imagesc(I);        
-        colormap gray;
-        hold on;
-        contour(??) %TODO 18: Line to complete
-        title('Image and zero level set of Phi')
+    if mod(nIter, 100)==0
+        %Plot the level sets surface
+        subplot(1,2,1) 
+            %The level set function
+            surfc(phi, 'LineStyle','none')  %TODO 16: Line to complete 
+            hold on
+            %The zero level set over the surface
+            contour(phi); %TODO 17: Line to complete
+            hold off
+            title('Phi Function');
 
-        axis off;
-        hold off
-    drawnow;
-    pause(.0001); 
+        %Plot the curve evolution over the image
+        subplot(1,2,2)
+            imagesc(I);        
+            colormap gray;
+            hold on;
+            contour(phi) %TODO 18: Line to complete
+            title('Image and zero level set of Phi')
+
+            axis off;
+            hold off
+        drawnow;
+        pause(.0001); 
+    end
 end
--- w3/sol_diracReg.m
+++ w3/sol_diracReg.m
@@ -1,7 +1,6 @@
-function y = sol_diracReg( x, epsilon )
-%  Dirac function of x
-%    sol_diracReg( x, epsilon ) Computes the derivative of the heaviside
-%    function of x with respect to x. Regularized based on epsilon.
+function y = sol_diracReg(x, eps)
+%Dirac function of x
+%   sol_diracReg( x, epsilon ) Computes the derivative of the heaviside
+%   function of x with respect to x. Regularized based on epsilon.
 
-y = ??; %TODO 19: Line to complete
-
+y = eps ./ (pi*(eps^2 + x.^2)); %TODO 19: Line to complete
